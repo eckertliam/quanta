70fa3699c5b765a83b8f5727c822876b07782bd7
Liam Eckert <liameckert17@gmail.com>
2024-08-09 14:32:56 -0400

    init

---
+++ build_parser.py
@@ -0,0 +1,14 @@
+# run commands to build the parser for compiler0
+from subprocess import call
+import os
+
+PARSER_DIR = "compiler0/generated"
+
+if os.path.exists(PARSER_DIR):
+    # remove the generated parser
+    for file in os.listdir(PARSER_DIR):
+        # if file is not __init__.py
+        if file != "__init__.py":
+            os.remove(os.path.join(PARSER_DIR, file))
+
+call(["antlr4", "-Dlanguage=Python3", "-o", PARSER_DIR, "Quanta.g4"])
---
+++ compiler0/README.md
@@ -0,0 +1,5 @@
+# Compiler 0: The Bootstrap Compiler
+## Author: Liam Eckert
+Compiler 0  is the compiler the compiles the compiler
+written in Quanta along with the stdlib. It is written in
+python because that is what will get Quanta off the ground fast.
