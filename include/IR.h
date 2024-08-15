56c4e4af032c07115e6810344b379d22efa499c6
Liam Eckert <liameckert17@gmail.com>
2024-08-15 08:45:02 -0400

    further expanding the IR prior to implementing the IR builder

--- include/IR.h
+++ include/IR.h
@@ -43,13 +43,37 @@ public:
 class Type : public IRNode {
 };
 
+class TypeVariable : public Type {
+public:
+    std::string ident;
+    std::vector<Type *> implements;
+
+    explicit TypeVariable(std::string ident) : ident(std::move(ident)) {}
+
+    void push_implement(Type *type) {
+        implements.push_back(type);
+    }
+
+    std::string to_string() override;
+};
+
+class TypeDecl : public IRNode {
+public:
+    std::string ident;
+    Type *type;
+
+    TypeDecl(std::string ident, Type *type) : ident(std::move(ident)), type(type) {}
+
+    std::string to_string() override;
+};
+
 class StructDecl : public IRNode {
 public:
     std::string ident;
-    std::vector<std::pair<std::string, Type *>> generics;
+    std::vector<TypeVariable *> generics;
     std::vector<std::pair<std::string, Type *>> fields;
 
-    StructDecl(std::string ident, std::vector<std::pair<std::string, Type *>> generics) : ident(std::move(ident)), generics(std::move(generics)) {}
+    explicit StructDecl(std::string ident) : ident(std::move(ident)) {}
 
     void push_field(const std::string& field_ident, Type *type) {
         fields.emplace_back(field_ident, type);
@@ -87,6 +111,7 @@ public:
 class Function : public IRNode {
 public:
     std::string ident;
+    std::vector<TypeVariable *> generics;
     std::vector<std::pair<std::string, Type *>> params;
     Type *return_type;
     Label *entry;
@@ -94,24 +119,25 @@ public:
 
     Function(std::string ident, std::vector<std::pair<std::string, Type *>> params, Type *return_type, Label *entry) : ident(std::move(ident)), params(std::move(params)), return_type(return_type), entry(entry) {}
 
+    void push_label(Label *label);
+    void push_generic(TypeVariable *type_variable);
+    void push_param(const std::string& ident, Type *type);
+
     std::string to_string() override;
 };
 
 class Module : public IRNode {
 public:
-    std::vector<StructDecl *> struct_decl;
+    std::vector<StructDecl *> structs;
+    std::vector<TypeDecl *> type_decls;
     std::vector<Function *> functions;
 
-    Module(std::vector<StructDecl *> struct_decl, std::vector<Function *> functions) : struct_decl(std::move(struct_decl)), functions(std::move(functions)) {}
+    Module(std::vector<StructDecl *> structs, std::vector<Function *> functions) : structs(std::move(structs)), functions(std::move(functions)) {}
     Module() = default;
 
-    void push_struct_decl(StructDecl *struct_decl) {
-        struct_decl.push_back(struct_decl);
-    }
-
-    void push_function(Function *function) {
-        functions.push_back(function);
-    }
+    void push_struct(StructDecl *struct_decl);
+    void push_function(Function *function);
+    void push_type_decl(TypeDecl *type_decl);
 
     std::string to_string() override;
 };
@@ -128,9 +154,11 @@ public:
     std::string to_string() override;
 };
 
-class BoolConstant : public IntConstant {
+class BoolConstant : public Constant {
 public:
-    explicit BoolConstant(bool value) : IntConstant(value) {}
+    bool value;
+
+    explicit BoolConstant(bool value) : value(value) {}
 
     std::string to_string() override;
 };
@@ -171,13 +199,8 @@ public:
     std::string to_string() override;
 };
 
-class Variable : public IRNode {
+class BoolType : public Type {
 public:
-    std::string ident;
-    Type *type;
-
-    Variable(std::string ident, Type *type) : ident(std::move(ident)), type(type) {}
-
     std::string to_string() override;
 };
 
@@ -231,19 +254,27 @@ public:
 class FunctionType : public Type {
 public:
     Type *return_type;
-    std::vector<Type *> args;
+    std::vector<Type *> generic_args;
+    std::vector<Type *> arg_types;
 
-    FunctionType(Type *return_type, std::vector<Type *> args) : return_type(return_type), args(args) {}
+    FunctionType(Type *return_type) : return_type(return_type) {}
+
+    void push_arg(Type *type);
+    void push_generic(Type *type);
 
     std::string to_string() override;
 };
 
-class StructType : public Type {
+class UserType : public Type {
 public:
-    StructDecl *struct_type;
-    std::vector<Type *> generics;
+    std::string ident;
+    std::vector<Type *> generic_args;
 
-    explicit StructType(StructDecl *struct_type) : struct_type(struct_type) {}
+    explicit UserType(std::string ident) : ident(std::move(ident)) {}
+
+    void push_generic(Type *type) {
+        generic_args.push_back(type);
+    }
 
     std::string to_string() override;
 };
--- src/IR.cpp
+++ src/IR.cpp
@@ -9,20 +9,51 @@ std::string Label::to_string() {
     for (auto &instruction : instructions) {
         result += TAB + instruction->to_string() + "\n";
     }
-    result += "}";
+    result += "}\n";
+    return result;
+}
+
+std::string TypeVariable::to_string() {
+    std::string result = ident;
+    if (!implements.empty()) {
+        result += " : ";
+        for (int i = 0; i < implements.size(); i++) {
+            result += implements[i]->to_string();
+            if (i != implements.size() - 1) {
+                result += ", ";
+            }
+        }
+    }
+    return result;
+}
+
+std::string TypeDecl::to_string() {
+    std::string result = "type " + ident + " = " + type->to_string() + ";\n";
+    return result;
 }
 
 std::string StructDecl::to_string() {
-    std::string result = "struct " + ident + " {\n";
+    std::string result = "struct " + ident;
+    if (!generics.empty()) {
+        result += "<";
+        for (int i = 0; i < generics.size(); i++) {
+            result += generics[i]->to_string();
+            if (i != generics.size() - 1) {
+                result += ", ";
+            }
+        }
+        result += ">";
+    }
+    result += " {\n";
     for (auto &field : fields) {
         result += TAB + field.first + ": " + field.second->to_string() + "\n";
     }
-    result += "}";
+    result += "}\n";
     return result;
 }
 
 std::string Goto::to_string() {
-    return "goto " + target->ident;
+    return "goto " + target->ident + ";\n";
 }
 
 std::string SwitchInt::to_string() {
@@ -37,7 +68,18 @@ std::string SwitchInt::to_string() {
 }
 
 std::string Function::to_string() {
-    std::string result = "fn " + ident + "(";
+    std::string result = "fn " + ident;
+    if (!generics.empty()) {
+        result += "<";
+        for (int i = 0; i < generics.size(); i++) {
+            result += generics[i]->to_string();
+            if (i != generics.size() - 1) {
+                result += ", ";
+            }
+        }
+        result += ">";
+    }
+    result += "(";
     // add comma until the last parameter
     for (int i = 0; i < params.size(); i++) {
         result += params[i].first + ": " + params[i].second->to_string();
@@ -50,6 +92,20 @@ std::string Function::to_string() {
         result += TAB + label->to_string() + "\n";
     }
     result += "}\n";
+    return result;
+}
+
+std::string Module::to_string() {
+    std::string result;
+    for (auto &struct_decl : structs) {
+        result += struct_decl->to_string() + "\n";
+    }
+    for (auto &type_decl : type_decls) {
+        result += type_decl->to_string() + "\n";
+    }
+    for (auto &function : functions) {
+        result += function->to_string() + "\n";
+    }
     return result;
 }
 
@@ -92,3 +148,57 @@ std::string StructConstant::to_string() {
     result += "}";
     return result;
 }
+
+std::string BoolType::to_string() {
+    return "bool";
+}
+
+std::string IntType::to_string() {
+    return "i" + std::to_string(size);
+}
+
+std::string FloatType::to_string() {
+    return "f" + std::to_string(size);
+}
+
+std::string PointerType::to_string() {
+    return "ptr<" + base->to_string() + ">";
+}
+
+std::string ArrayType::to_string() {
+    return "[" + std::to_string(size) + " : " + base->to_string() + "]";
+}
+
+std::string CharType::to_string() {
+    return "char";
+}
+
+std::string VoidType::to_string() {
+    return "void";
+}
+
+std::string FunctionType::to_string() {
+    std::string result = "Fn";
+    if (!generic_args.empty()) {
+        result += "<";
+        for (int i = 0; i < generic_args.size(); i++) {
+            result += generic_args[i]->to_string();
+            if (i != generic_args.size() - 1) {
+                result += ", ";
+            }
+        }
+        result += ">";
+    }
+    result += "(";
+    for (int i = 0; i < arg_types.size(); i++) {
+        result += arg_types[i]->to_string();
+        if (i != arg_types.size() - 1) {
+            result += ", ";
+        }
+    }
+    result += ") -> " + return_type->to_string();
+    return result;
+}
+
+
+
