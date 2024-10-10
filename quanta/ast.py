01fc96bf0a8e480cd028bebded5f9c6259e4cc98
Liam Eckert <liameckert17@gmail.com>
2024-10-10 07:53:13 -0400

    added poetry and basic ast

--- quanta/ast.py
+++ quanta/ast.py
@@ -1,5 +1,6 @@
-from typing import Union
+from typing import Union, List
 from enum import Enum, auto
+
 
 class Loc:
     def __init__(self, line: int, col: int):
@@ -7,45 +8,31 @@ class Loc:
         self.col = col
 
 
-class AstNode:
-    def __init__(self, start: Loc, end: Loc):
-        self.start = start
-        self.end = end
-        
-        
-class AtomType(Enum):
+class ExprType(Enum):
     INT = auto()
     FLOAT = auto()
     STRING = auto()
     SYMBOL = auto()
     BOOL = auto()
-    NULL = auto()    
+    LIST = auto()
+    NULL = auto()
     
-def well_formed_atom(value: any, atom_type: AtomType) -> bool:
-    if atom_type == AtomType.INT:
-        return isinstance(value, int)
-    elif atom_type == AtomType.FLOAT:
-        return isinstance(value, float)
-    elif atom_type == AtomType.STRING:
-        return isinstance(value, str)
-    elif atom_type == AtomType.SYMBOL:
-        return isinstance(value, str)
-    elif atom_type == AtomType.BOOL:
-        return isinstance(value, bool)
-    elif atom_type == AtomType.NULL:
-        return value is None
-    else:
-        return False
-    
-class Atom(AstNode):
-    def __init__(self, value: any, atom_type: AtomType, start: Loc, end: Loc):
-        super().__init__(start, end)
-        assert well_formed_atom(value, atom_type)
-        self.value = value
-        self.atom_type = atom_type
-        
 
-class List(AstNode):
-    def __init__(self, elements: list, start: Loc, end: Loc):
-        super().__init__(start, end)
-        self.elements = elements
+ExprValue = Union[int, float, str, bool, None, List['Expr']]
+    
+
+class Expr:
+    def __init__(self, value: ExprValue, expr_type: ExprType, start: Loc, end: Loc):
+        self.start = start
+        self.end = end
+        self.value = value
+        self.expr_type = expr_type
+        
+    def __str__(self):
+        if self.expr_type == ExprType.LIST:
+            return f"({' '.join(map(str, self.value.elements))})"
+        elif self.expr_type == ExprType.STRING:
+            return f'"{self.value.value}"'
+        else:
+            return str(self.value.value)
+    
