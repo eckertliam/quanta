b801ddb60aafdea7f172517b190dec4e58584531
Liam Eckert <liameckert17@gmail.com>
2024-10-11 16:12:13 -0400

    Reworking the ast and building out the target language example

--- examples/hello_world.q
+++ examples/hello_world.q
@@ -1,9 +1,14 @@
 fn add
-    a: int
-    b: int
-    -> int
+    a: i32
+    b: i32
+    -> i32
 begin
     return a + b;
+end
+
+struct Point
+    x: i32
+    y: i32
 end
 
 fn main
--- quanta/ast.py
+++ quanta/ast.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 from enum import Enum, auto
 
 
@@ -13,75 +13,235 @@ class AstNode:
         self.loc = loc
 
 
-class Atom(AstNode):
-    def __init__(self, value: any, loc: Loc):
+class AstStmt(AstNode):
+    pass
+
+class AstExpr(AstNode):
+    pass
+
+class AstType(AstNode):
+    pass
+
+class AstBlock(AstStmt):
+    def __init__(self, stmts: List[AstStmt], loc: Loc):
+        super().__init__(loc)
+        self.stmts = stmts
+
+    def push(self, stmt: AstStmt):
+        self.stmts.append(stmt)
+
+class AstConstDecl(AstStmt):
+    def __init__(self, name: str, ty: AstType, value: AstExpr, loc: Loc):
+        super().__init__(loc)
+        self.name = name
+        self.ty = ty
+        self.value = value
+
+class AstLetDecl(AstStmt):
+    def __init__(self, name: str, ty: AstType, value: AstExpr, loc: Loc):
+        super().__init__(loc)
+        self.name = name
+        self.ty = ty
+        self.value = value
+
+class AstVarDecl:
+    @staticmethod
+    def __call__(name: str, mut: bool, ty: AstType, value: AstExpr, loc: Loc) -> Union[AstConstDecl, AstLetDecl]:
+        if mut:
+            return AstLetDecl(name, ty, value, loc)
+        else:
+            return AstConstDecl(name, ty, value, loc)
+
+class AstTypeDecl(AstStmt):
+    def __init__(self, name: str, generics: List[str], ty: AstType, loc: Loc):
+        super().__init__(loc)
+        self.name = name
+        self.generics = generics
+        self.ty = ty
+
+class AstStructField:
+    def __init__(self, name: str, ty: AstType, loc: Loc):
+        self.name = name
+        self.ty = ty
+        self.loc = loc
+
+class AstStructDecl(AstStmt):
+    def __init__(self, name: str, generics: List[str], fields: List[AstStructField], loc: Loc):
+        super().__init__(loc)
+        self.name = name
+        self.generics = generics
+        self.fields = fields
+
+class AstFnParam:
+    def __init__(self, name: str, ty: AstType, loc: Loc):
+        self.name = name
+        self.ty = ty
+        self.loc = loc
+
+class AstFnDecl(AstStmt):
+    def __init__(self, name: str, generics: List[str], params: List[AstFnParam], ret_ty: AstType, body: AstBlock, loc: Loc):
+        super().__init__(loc)
+        self.name = name
+        self.generics = generics
+        self.params = params
+        self.ret_ty = ret_ty
+        self.body = body
+
+class AstIfStmt(AstStmt):
+    def __init__(self, cond: AstExpr, then_block: AstBlock, else_block: Optional[AstBlock], loc: Loc):
+        super().__init__(loc)
+        self.cond = cond
+        self.then_block = then_block
+        self.else_block = else_block
+
+class AstLambdaStmt(AstStmt):
+    def __init__(self, params: List[AstFnParam], ret_ty: AstType, body: AstBlock, loc: Loc):
+        super().__init__(loc)
+        self.params = params
+        self.ret_ty = ret_ty
+        self.body = body
+
+class AstReturnStmt(AstStmt):
+    def __init__(self, value: AstExpr, loc: Loc):
         super().__init__(loc)
         self.value = value
 
-class Int(Atom):
+class AstBinOpKind(Enum):
+    ADD = auto()
+    SUB = auto()
+    MUL = auto()
+    DIV = auto()
+    MOD = auto()
+    AND = auto()
+    OR = auto()
+    EQ = auto()
+    NE = auto()
+    LT = auto()
+    LE = auto()
+    GT = auto()
+    GE = auto()
+
+class AstBinOp(AstExpr):
+    def __init__(self, kind: AstBinOpKind, lhs: AstExpr, rhs: AstExpr, loc: Loc):
+        super().__init__(loc)
+        self.kind = kind
+        self.lhs = lhs
+        self.rhs = rhs
+
+class AstUnOpKind(Enum):
+    NEG = auto()
+    NOT = auto()
+
+class AstUnOp(AstExpr):
+    def __init__(self, kind: AstUnOpKind, expr: AstExpr, loc: Loc):
+        super().__init__(loc)
+        self.kind = kind
+        self.expr = expr
+
+class AstCall(AstExpr):
+    def __init__(self, callee: AstExpr, args: List[AstExpr], loc: Loc):
+        super().__init__(loc)
+        self.callee = callee
+        self.args = args
+
+class AstSymbol(AstExpr):
+    def __init__(self, name: str, loc: Loc):
+        super().__init__(loc)
+        self.name = name
+
+class AstInt(AstExpr):
     def __init__(self, value: int, loc: Loc):
-        super().__init__(value, loc)
+        super().__init__(loc)
+        self.value = value
 
-
-class Float(Atom):
+class AstFloat(AstExpr):
     def __init__(self, value: float, loc: Loc):
-        super().__init__(value, loc)
+        super().__init__(loc)
+        self.value = value
 
-
-class String(Atom):
-    def __init__(self, value: str, loc: Loc):
-        super().__init__(value, loc)
-
-
-class Bool(Atom):
+class AstBool(AstExpr):
     def __init__(self, value: bool, loc: Loc):
-        super().__init__(value, loc)
+        super().__init__(loc)
+        self.value = value
 
-
-class Symbol(Atom):
+class AstString(AstExpr):
     def __init__(self, value: str, loc: Loc):
-        super().__init__(value, loc)
+        super().__init__(loc)
+        self.value = value
 
-
-class Delimiter(Enum):
-    Paren = auto()
-    Bracket = auto()
-    Brace = auto()
-
-    def open(self):
-        if self == Delimiter.Paren:
-            return '('
-        elif self == Delimiter.Bracket:
-            return '['
-        elif self == Delimiter.Brace:
-            return '{'
-
-    def close(self):
-        if self == Delimiter.Paren:
-            return ')'
-        elif self == Delimiter.Bracket:
-            return ']'
-        elif self == Delimiter.Brace:
-            return '}'
-
-    @staticmethod
-    def as_delim(delim: str) -> Optional['Delimiter']:
-        if delim == '(':
-            return Delimiter.Paren
-        elif delim == '[':
-            return Delimiter.Bracket
-        elif delim == '{':
-            return Delimiter.Brace
-        else:
-            return None
-
-
-class Form(AstNode):
-    """
-    A form is a list of elements
-    Refined into an array, vec, function call, etc. during MIR generation
-    """
-    def __init__(self, elements: List[AstNode], delim: Delimiter, loc: Loc):
+class AstArray(AstExpr):
+    def __init__(self, elements: List[AstExpr], loc: Loc):
         super().__init__(loc)
         self.elements = elements
-        self.delim = delim
+
+class AstStructLiteral(AstExpr):
+    def __init__(self, fields: List[AstExpr], loc: Loc):
+        super().__init__(loc)
+        self.fields = fields
+
+class AstFieldAccess(AstExpr):
+    def __init__(self, expr: AstExpr, field: str, loc: Loc):
+        super().__init__(loc)
+        self.expr = expr
+        self.field = field
+
+class AstIndexAccess(AstExpr):
+    def __init__(self, expr: AstExpr, index: AstExpr, loc: Loc):
+        super().__init__(loc)
+        self.expr = expr
+        self.index = index
+
+class AstExprBlock(AstExpr):
+    def __init__(self, expr: AstExpr, loc: Loc):
+        super().__init__(loc)
+        self.expr = expr
+
+class AstTypeInt(AstType):
+    def __init__(self, size: int, loc: Loc):
+        super().__init__(loc)
+        self.size = size
+        assert size in [8, 16, 32, 64]
+
+class AstTypeFloat(AstType):
+    def __init__(self, size: int, loc: Loc):
+        super().__init__(loc)
+        self.size = size
+        assert size in [32, 64]
+
+class AstTypeBool(AstType):
+    def __init__(self, loc: Loc):
+        super().__init__(loc)
+
+class AstTypeString(AstType):
+    def __init__(self, loc: Loc):
+        super().__init__(loc)
+
+class AstTypeArray(AstType):
+    def __init__(self, elem_ty: AstType, loc: Loc):
+        super().__init__(loc)
+        self.elem_ty = elem_ty
+
+class AstTypeStruct(AstType):
+    def __init__(self, name: str, generics: List[AstType], loc: Loc):
+        super().__init__(loc)
+        self.name = name
+        self.generics = generics
+
+class AstTypeFn(AstType):
+    def __init__(self, params: List[AstType], ret_ty: AstType, loc: Loc):
+        super().__init__(loc)
+        self.params = params
+        self.ret_ty = ret_ty
+
+class AstTypeGeneric(AstType):
+    def __init__(self, name: str, loc: Loc):
+        super().__init__(loc)
+        self.name = name
+
+class AstTypeVoid(AstType):
+    def __init__(self, loc: Loc):
+        super().__init__(loc)
+
+class AstTypeAny(AstType):
+    def __init__(self, loc: Loc):
+        super().__init__(loc)
