94bb252a7bc68d40de90a64f3317bbf8f36b9c13
Liam Eckert <liameckert17@gmail.com>
2024-10-13 15:21:20 -0400

    Working lexer

--- quanta/ast.py
+++ quanta/ast.py
@@ -1,11 +1,17 @@
-from typing import List, Optional, Union
 from enum import Enum, auto
+from typing import List, Optional, Union
 
 
 class Loc:
     def __init__(self, line: int, col: int):
         self.line = line
         self.col = col
+
+    def __str__(self):
+        return f"{self.line}:{self.col}"
+
+    def __repr__(self):
+        return str(self)
 
 
 class Node:
@@ -16,11 +22,14 @@ class Node:
 class Statement(Node):
     pass
 
+
 class Expression(Node):
     pass
 
+
 class Type(Node):
     pass
+
 
 class Block(Statement):
     def __init__(self, stmts: List[Statement], loc: Loc):
@@ -30,6 +39,7 @@ class Block(Statement):
     def push(self, stmt: Statement):
         self.stmts.append(stmt)
 
+
 class ConstDecl(Statement):
     def __init__(self, name: str, ty: Type, value: Expression, loc: Loc):
         super().__init__(loc)
@@ -37,12 +47,14 @@ class ConstDecl(Statement):
         self.ty = ty
         self.value = value
 
+
 class LetDecl(Statement):
     def __init__(self, name: str, ty: Type, value: Expression, loc: Loc):
         super().__init__(loc)
         self.name = name
         self.ty = ty
         self.value = value
+
 
 class VarDecl:
     @staticmethod
@@ -52,6 +64,7 @@ class VarDecl:
         else:
             return ConstDecl(name, ty, value, loc)
 
+
 class TypeDecl(Statement):
     def __init__(self, name: str, generics: List[str], ty: Type, loc: Loc):
         super().__init__(loc)
@@ -59,11 +72,13 @@ class TypeDecl(Statement):
         self.generics = generics
         self.ty = ty
 
+
 class StructField:
     def __init__(self, name: str, ty: Type, loc: Loc):
         self.name = name
         self.ty = ty
         self.loc = loc
+
 
 class StructDecl(Statement):
     def __init__(self, name: str, generics: List[str], fields: List[StructField], loc: Loc):
@@ -72,11 +87,13 @@ class StructDecl(Statement):
         self.generics = generics
         self.fields = fields
 
+
 class FnParam:
     def __init__(self, name: str, ty: Type, loc: Loc):
         self.name = name
         self.ty = ty
         self.loc = loc
+
 
 class FnDecl(Statement):
     def __init__(self, name: str, generics: List[str], params: List[FnParam], ret_ty: Type, body: Block, loc: Loc):
@@ -87,12 +104,14 @@ class FnDecl(Statement):
         self.ret_ty = ret_ty
         self.body = body
 
+
 class IfStmt(Statement):
     def __init__(self, cond: Expression, then_block: Block, else_block: Optional[Block], loc: Loc):
         super().__init__(loc)
         self.cond = cond
         self.then_block = then_block
         self.else_block = else_block
+
 
 class LambdaStmt(Statement):
     def __init__(self, params: List[FnParam], ret_ty: Type, body: Block, loc: Loc):
@@ -101,10 +120,12 @@ class LambdaStmt(Statement):
         self.ret_ty = ret_ty
         self.body = body
 
+
 class ReturnStmt(Statement):
     def __init__(self, value: Expression, loc: Loc):
         super().__init__(loc)
         self.value = value
+
 
 class BinOpKind(Enum):
     ADD = auto()
@@ -121,6 +142,7 @@ class BinOpKind(Enum):
     GT = auto()
     GE = auto()
 
+
 class BinOp(Expression):
     def __init__(self, kind: BinOpKind, lhs: Expression, rhs: Expression, loc: Loc):
         super().__init__(loc)
@@ -128,9 +150,11 @@ class BinOp(Expression):
         self.lhs = lhs
         self.rhs = rhs
 
+
 class UnOpKind(Enum):
     NEG = auto()
     NOT = auto()
+
 
 class UnOp(Expression):
     def __init__(self, kind: UnOpKind, expr: Expression, loc: Loc):
@@ -138,46 +162,55 @@ class UnOp(Expression):
         self.kind = kind
         self.expr = expr
 
+
 class FnCall(Expression):
     def __init__(self, callee: Expression, args: List[Expression], loc: Loc):
         super().__init__(loc)
         self.callee = callee
         self.args = args
 
+
 class Symbol(Expression):
     def __init__(self, name: str, loc: Loc):
         super().__init__(loc)
         self.name = name
+
 
 class Int(Expression):
     def __init__(self, value: int, loc: Loc):
         super().__init__(loc)
         self.value = value
 
+
 class Float(Expression):
     def __init__(self, value: float, loc: Loc):
         super().__init__(loc)
         self.value = value
+
 
 class Bool(Expression):
     def __init__(self, value: bool, loc: Loc):
         super().__init__(loc)
         self.value = value
 
+
 class String(Expression):
     def __init__(self, value: str, loc: Loc):
         super().__init__(loc)
         self.value = value
+
 
 class Array(Expression):
     def __init__(self, elements: List[Expression], loc: Loc):
         super().__init__(loc)
         self.elements = elements
 
+
 class StructLiteral(Expression):
     def __init__(self, fields: List[Expression], loc: Loc):
         super().__init__(loc)
         self.fields = fields
+
 
 class FieldAccess(Expression):
     def __init__(self, expr: Expression, field: str, loc: Loc):
@@ -185,16 +218,19 @@ class FieldAccess(Expression):
         self.expr = expr
         self.field = field
 
+
 class IndexAccess(Expression):
     def __init__(self, expr: Expression, index: Expression, loc: Loc):
         super().__init__(loc)
         self.expr = expr
         self.index = index
 
+
 class ExprBlock(Expression):
     def __init__(self, expr: Expression, loc: Loc):
         super().__init__(loc)
         self.expr = expr
+
 
 class IntType(Type):
     def __init__(self, size: int, loc: Loc):
@@ -202,24 +238,29 @@ class IntType(Type):
         self.size = size
         assert size in [8, 16, 32, 64]
 
+
 class FloatType(Type):
     def __init__(self, size: int, loc: Loc):
         super().__init__(loc)
         self.size = size
         assert size in [32, 64]
 
+
 class BoolType(Type):
     def __init__(self, loc: Loc):
         super().__init__(loc)
+
 
 class StringType(Type):
     def __init__(self, loc: Loc):
         super().__init__(loc)
 
+
 class ArrayType(Type):
     def __init__(self, elem_ty: Type, loc: Loc):
         super().__init__(loc)
         self.elem_ty = elem_ty
+
 
 class StructType(Type):
     def __init__(self, name: str, generics: List[Type], loc: Loc):
@@ -227,16 +268,19 @@ class StructType(Type):
         self.name = name
         self.generics = generics
 
+
 class FnType(Type):
     def __init__(self, params: List[Type], ret_ty: Type, loc: Loc):
         super().__init__(loc)
         self.params = params
         self.ret_ty = ret_ty
 
+
 class TypeVar(Type):
     def __init__(self, name: str, loc: Loc):
         super().__init__(loc)
         self.name = name
+
 
 class VoidType(Type):
     def __init__(self, loc: Loc):
