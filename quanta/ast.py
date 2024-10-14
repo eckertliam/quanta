9b095425aa57cda1f46687a331f29ab7c6ca8b03
Liam Eckert <liameckert17@gmail.com>
2024-10-13 20:14:06 -0400

    Nodes now use Spans instead of a starting Loc

--- quanta/ast.py
+++ quanta/ast.py
@@ -14,9 +14,21 @@ class Loc:
         return str(self)
 
 
+class Span:
+    def __init__(self, start: Loc, end: Loc):
+        self.start = start
+        self.end = end
+
+    def __str__(self):
+        return f"{self.start} - {self.end}"
+
+    def __repr__(self):
+        return str(self)
+
+
 class Node:
-    def __init__(self, loc: Loc):
-        self.loc = loc
+    def __init__(self, start: Loc, end: Loc):
+        self.span = Span(start, end)
 
 class Program:
     def __init__(self, stmts: List['Statement']):
@@ -38,8 +50,10 @@ class Type(Node):
 
 
 class Block(Statement):
-    def __init__(self, stmts: List[Statement], loc: Loc):
-        super().__init__(loc)
+    def __init__(self, start: Loc, end: Loc, stmts: List[Statement]=None):
+        super().__init__(start, end)
+        if stmts is None:
+            stmts = []
         self.stmts = stmts
 
     def push(self, stmt: Statement):
@@ -47,16 +61,16 @@ class Block(Statement):
 
 
 class ConstDecl(Statement):
-    def __init__(self, name: str, ty: Type, value: Expression, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, name: str, ty: Type, value: Expression, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
         self.ty = ty
         self.value = value
 
 
 class LetDecl(Statement):
-    def __init__(self, name: str, ty: Type, value: Expression, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, name: str, ty: Type, value: Expression, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
         self.ty = ty
         self.value = value
@@ -64,46 +78,46 @@ class LetDecl(Statement):
 
 class VarDecl:
     @staticmethod
-    def init(name: str, mut: bool, ty: Type, value: Expression, loc: Loc) -> Union[ConstDecl, LetDecl]:
+    def init(name: str, mut: bool, ty: Type, value: Expression, start: Loc, end: Loc) -> Union[ConstDecl, LetDecl]:
         if mut:
-            return LetDecl(name, ty, value, loc)
+            return LetDecl(name, ty, value, start, end)
         else:
-            return ConstDecl(name, ty, value, loc)
+            return ConstDecl(name, ty, value, start, end)
 
 
 class TypeDecl(Statement):
-    def __init__(self, name: str, generics: List[str], ty: Type, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, name: str, generics: List[str], ty: Type, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
         self.generics = generics
         self.ty = ty
 
 
-class StructField:
-    def __init__(self, name: str, ty: Type, loc: Loc):
+class StructField(Node):
+    def __init__(self, name: str, ty: Type, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
         self.ty = ty
-        self.loc = loc
 
 
 class StructDecl(Statement):
-    def __init__(self, name: str, generics: List[str], fields: List[StructField], loc: Loc):
-        super().__init__(loc)
+    def __init__(self, name: str, generics: List[str], fields: List[StructField], start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
         self.generics = generics
         self.fields = fields
 
 
-class FnParam:
-    def __init__(self, name: str, ty: Type, loc: Loc):
+class FnParam(Node):
+    def __init__(self, name: str, ty: Type, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
         self.ty = ty
-        self.loc = loc
-
+        self.start = start
 
 class FnDecl(Statement):
-    def __init__(self, name: str, generics: List[str], params: List[FnParam], ret_ty: Type, body: Block, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, name: str, generics: List[str], params: List[FnParam], ret_ty: Type, body: Block, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
         self.generics = generics
         self.params = params
@@ -112,24 +126,24 @@ class FnDecl(Statement):
 
 
 class IfStmt(Statement):
-    def __init__(self, cond: Expression, then_block: Block, else_block: Optional[Block], loc: Loc):
-        super().__init__(loc)
+    def __init__(self, cond: Expression, then_block: Block, else_block: Optional[Block], start: Loc, end: Loc):
+        super().__init__(start, end)
         self.cond = cond
         self.then_block = then_block
         self.else_block = else_block
 
 
 class LambdaStmt(Statement):
-    def __init__(self, params: List[FnParam], ret_ty: Type, body: Block, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, params: List[FnParam], ret_ty: Type, body: Block, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.params = params
         self.ret_ty = ret_ty
         self.body = body
 
 
 class ReturnStmt(Statement):
-    def __init__(self, value: Expression, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, value: Expression, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.value = value
 
 
@@ -150,8 +164,8 @@ class BinOpKind(Enum):
 
 
 class BinOp(Expression):
-    def __init__(self, kind: BinOpKind, lhs: Expression, rhs: Expression, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, kind: BinOpKind, lhs: Expression, rhs: Expression, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.kind = kind
         self.lhs = lhs
         self.rhs = rhs
@@ -163,131 +177,131 @@ class UnOpKind(Enum):
 
 
 class UnOp(Expression):
-    def __init__(self, kind: UnOpKind, expr: Expression, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, kind: UnOpKind, expr: Expression, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.kind = kind
         self.expr = expr
 
 
 class FnCall(Expression):
-    def __init__(self, callee: Expression, args: List[Expression], loc: Loc):
-        super().__init__(loc)
+    def __init__(self, callee: Expression, args: List[Expression], start: Loc, end: Loc):
+        super().__init__(start, end)
         self.callee = callee
         self.args = args
 
 
 class Symbol(Expression):
-    def __init__(self, name: str, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, name: str, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
 
 
 class Int(Expression):
-    def __init__(self, value: int, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, value: int, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.value = value
 
 
 class Float(Expression):
-    def __init__(self, value: float, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, value: float, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.value = value
 
 
 class Bool(Expression):
-    def __init__(self, value: bool, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, value: bool, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.value = value
 
 
 class String(Expression):
-    def __init__(self, value: str, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, value: str, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.value = value
 
 
 class Array(Expression):
-    def __init__(self, elements: List[Expression], loc: Loc):
-        super().__init__(loc)
+    def __init__(self, elements: List[Expression], start: Loc, end: Loc):
+        super().__init__(start, end)
         self.elements = elements
 
 
 class StructLiteral(Expression):
-    def __init__(self, fields: List[Expression], loc: Loc):
-        super().__init__(loc)
+    def __init__(self, fields: List[Expression], start: Loc, end: Loc):
+        super().__init__(start, end)
         self.fields = fields
 
 
 class FieldAccess(Expression):
-    def __init__(self, expr: Expression, field: str, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, expr: Expression, field: str, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.expr = expr
         self.field = field
 
 
 class IndexAccess(Expression):
-    def __init__(self, expr: Expression, index: Expression, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, expr: Expression, index: Expression, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.expr = expr
         self.index = index
 
 
 class ExprBlock(Expression):
-    def __init__(self, expr: Expression, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, expr: Expression, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.expr = expr
 
 
 class IntType(Type):
-    def __init__(self, size: int, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, size: int, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.size = size
         assert size in [8, 16, 32, 64]
 
 
 class FloatType(Type):
-    def __init__(self, size: int, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, size: int, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.size = size
         assert size in [32, 64]
 
 
 class BoolType(Type):
-    def __init__(self, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, start: Loc, end: Loc):
+        super().__init__(start, end)
 
 
 class StringType(Type):
-    def __init__(self, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, start: Loc, end: Loc):
+        super().__init__(start, end)
 
 
 class ArrayType(Type):
-    def __init__(self, elem_ty: Type, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, elem_ty: Type, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.elem_ty = elem_ty
 
 
 class StructType(Type):
-    def __init__(self, name: str, generics: List[Type], loc: Loc):
-        super().__init__(loc)
+    def __init__(self, name: str, generics: List[Type], start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
         self.generics = generics
 
 
 class FnType(Type):
-    def __init__(self, params: List[Type], ret_ty: Type, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, params: List[Type], ret_ty: Type, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.params = params
         self.ret_ty = ret_ty
 
 
 class TypeVar(Type):
-    def __init__(self, name: str, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, name: str, start: Loc, end: Loc):
+        super().__init__(start, end)
         self.name = name
 
 
 class VoidType(Type):
-    def __init__(self, loc: Loc):
-        super().__init__(loc)
+    def __init__(self, start: Loc, end: Loc):
+        super().__init__(start, end)
