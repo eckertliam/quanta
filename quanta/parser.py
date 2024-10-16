94e29c7f9f075362a57cba205a921c3ed457f80b
Liam Eckert <liameckert17@gmail.com>
2024-10-16 19:49:54 -0400

    Update parser.py

--- quanta/parser.py
+++ quanta/parser.py
@@ -1,117 +1,112 @@
+from typing import Optional, Callable, List
 import quanta.ast as ast
-from quanta.ast import Loc
-from quanta.lexer import Token, TokenKind, TokenIter
-from typing import List, Optional
 
-def parse_var_decl(tokens: TokenIter, mut: bool) -> ast.Statement:
-    start_loc = tokens.peek(-1).loc
-    try:
-        name = tokens.match(TokenKind.SYMBOL).lexeme
-    except:
-        token = tokens.peek()
-        raise Exception(f"Expected symbol, got {token}")
-    ty: Optional[ast.Type] = None
-    if tokens.match(TokenKind.COLON):
-        try:
-            ty = parse_type(tokens)
-        except Exception as e:
-            raise Exception(f"Expected a type after semicolon at {tokens.peek().loc} {e}")
-    if not tokens.match(TokenKind.EQUAL):
-        raise Exception(f"Expected = after {"let" if mut else "const"} declaration at {tokens.peek().loc}")
-    try:
-        value = parse_expr(tokens)
-    except Exception as e:
-        raise Exception(f"Expected an expression after = at {tokens.peek().loc} {e}")
-    end_loc = tokens.peek(-1).loc
-    return ast.VarDecl.init(name, mut, ty, value, start_loc, end_loc)
 
-def parse_block(tokens: TokenIter) -> ast.Block:
-    start_loc = tokens.peek(-1).loc
-    stmts = []
-    while not (tokens.match(TokenKind.END) or tokens.eof()):
-        stmts.append(parse_stmt(tokens))
-    if tokens.eof():
-        raise Exception(f"Expected end of block at {tokens.peek().loc}")
+class Chars:
+    """
+    A Char iterator that can be used to parse characters from a string.
+    """
+
+    def __init__(self, src: str):
+        self.src = src
+        self.pos = 0
+        self.line = 1
+        self.col = 1
+
+    def loc(self) -> ast.Loc:
+        return ast.Loc(self.line, self.col)
+
+    def is_done(self) -> bool:
+        return self.pos >= len(self.src)
+
+    def peek(self) -> Optional[str]:
+        """
+        Peek the next character without consuming it.
+        """
+        if self.is_done():
+            return None
+        return self.src[self.pos]
+
+    def consume(self) -> Optional[str]:
+        """
+        Consume the next character and return it.
+        """
+        if self.is_done():
+            return None
+        self.pos += 1
+        self.col += 1
+        if self.src[self.pos - 1] == "\n":
+            self.line += 1
+            self.col = 1
+        return self.src[self.pos - 1]
+    
+    def consume_whitespace(self) -> None:
+        """
+        Consume whitespace characters from the current position.
+        """
+        while not self.is_done() and self.peek().isspace():
+            self.consume()
+
+def parse_sexpr(chars: Chars, close_char: str, start_loc: ast.Loc, delim_kind: ast.DelimKind) -> ast.SExpr:
+    chars.consume()
+    chars.consume_whitespace()
+    elements: List[ast.Expr] = []
+    while True:
+        chars.consume_whitespace()
+        if chars.peek() == close_char:
+            chars.consume()
+            break
+        elif chars.peek() == None:
+            raise Exception(f"Unexpected EOF while parsing {delim_kind} delimited expression at {start_loc}")
+        else:
+            elements.append(parse_expr(chars))
+    return ast.SExpr(ast.Span(start_loc, chars.loc()), elements, delim_kind)
+    
+
+def parse_string(chars: Chars, start_loc: ast.Loc) -> ast.String:
+    # skip the opening quote
+    chars.consume()
+    value = ""
+    while not chars.is_done() and chars.peek() != "\"":
+        if chars.peek() == "\\":
+            # skip the escape character
+            chars.consume()
+            # catch the unlikely case where the file ends with an escape character in the middle of a string :/
+            if chars.is_done():
+                raise Exception(f"Unexpected EOF while parsing STRING at {start_loc}")
+        # consume the character
+        value += chars.consume()
+    # confirm there is a closing quote
+    if chars.peek() != "\"":
+        raise Exception(f"Unterminated string at {start_loc}")
     else:
-        end_loc = tokens.peek(-1).loc
-        return ast.Block(start_loc, end_loc, stmts)
+        # consume the closing quote
+        chars.consume()
+        return ast.String(ast.Span(start_loc, chars.loc()), value)
 
-def parse_if(tokens: TokenIter) -> ast.IfStmt:
-    start_loc = tokens.peek(-1).loc
-    try:
-        cond = parse_expr(tokens)
-    except Exception as e:
-        raise Exception(f"Expected an expression after if at {tokens.peek().loc} {e}")
-    try:
-        then_block = parse_block(tokens)
-    except Exception as e:
-        raise Exception(f"Expected a block after if condition at {tokens.peek().loc} {e}")
-    else_block = None
-    if tokens.match(TokenKind.ELSE):
-        try:
-            if tokens.match(TokenKind.IF):
-                else_block = parse_if(tokens)
-            else:
-                else_block = parse_block(tokens)
-        except Exception as e:
-            raise Exception(f"Expected a block or if-else statement after else at {tokens.peek().loc} {e}")
-    end_loc = tokens.peek(-1).loc
-    return ast.IfStmt(cond, then_block, else_block, start_loc, end_loc)
 
-def parse_generic_list(tokens: TokenIter) -> List[str]:
-    generics = []
-    if tokens.match(TokenKind.LT):
-        while not (tokens.match(TokenKind.GT) or tokens.eof()):
+def parse_expr(chars: Chars) -> ast.Expr:
+    chars.consume_whitespace()
+    char: str = chars.peek()
+    start_loc: ast.Loc = chars.loc()
+    match char:
+        case "(":
             try:
-                generics.append(tokens.match(TokenKind.SYMBOL).lexeme)
+                return parse_sexpr(chars, ")", start_loc, ast.DelimKind.PAREN)
             except Exception as e:
-                raise Exception(f"Expected a symbol in generics list at {tokens.peek().loc} {e}")
-        if tokens.eof():
-            raise Exception(f"Expected > at {tokens.peek().loc}")
-    return generics
-
-def parse_type_decl(tokens: TokenIter) -> ast.TypeDecl:
-    start_loc = tokens.peek(-1).loc
-    try:
-        name = tokens.match(TokenKind.SYMBOL).lexeme
-    except:
-        token = tokens.peek()
-        raise Exception(f"Expected symbol, got {token}")
-    generics = parse_generic_list(tokens)
-    if not tokens.match(TokenKind.EQUAL):
-        raise Exception(f"Expected = after type declaration at {tokens.peek().loc}")
-    try:
-        ty = parse_type(tokens)
-    except Exception as e:
-        raise Exception(f"Expected a type after = at {tokens.peek().loc} {e}")
-    end_loc = tokens.peek(-1).loc
-    return ast.TypeDecl(name, generics, ty, start_loc, end_loc)
-
-def parse_stmt(tokens: TokenIter) -> ast.Statement:
-    # is it prefixed with pub?
-    pub = None != tokens.match(TokenKind.PUB)
-    if tokens.match(TokenKind.CONST):
-        return parse_var_decl(tokens, False)
-    elif tokens.match(TokenKind.LET):
-        return parse_var_decl(tokens, True)
-    elif tokens.match(TokenKind.BEGIN):
-        return parse_block(tokens)
-    elif tokens.match(TokenKind.IF):
-        return parse_if(tokens)
-    elif tokens.match(TokenKind.TYPE):
-        return parse_type_decl(tokens)
-
-
-def parse_type(tokens: TokenIter) -> ast.Type:
-    # TODO: unimplemented
-    pass
-
-def parse_expr(tokens: TokenIter) -> ast.Expression:
-    # TODO: unimplemented
-    pass
-
-def parse(tokens: TokenIter) -> ast.Program:
-    prog = ast.Program([])
-    while not tokens.eof():
-        stmt = parse_stmt(tokens)
-        prog.push(stmt)
+                raise Exception(f"Unexpected error while parsing a list at {start_loc}: {e}")
+        case "[":
+            try:
+                return parse_sexpr(chars, "]", start_loc, ast.DelimKind.BRACKET)
+            except Exception as e:
+                raise Exception(f"Unexpected error while parsing a list at {start_loc}: {e}")
+        case "{":
+            try:
+                return parse_sexpr(chars, "}", start_loc, ast.DelimKind.BRACE)
+            except Exception as e:
+                raise Exception(f"Unexpected error while parsing a list at {start_loc}: {e}")
+        case "\"":
+            try:
+                return parse_string(chars, start_loc)
+            except Exception as e:
+                raise Exception(f"Unexpected error while parsing a string at {start_loc}: {e}")
